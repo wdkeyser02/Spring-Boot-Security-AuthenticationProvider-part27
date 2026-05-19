36f76d3910d3f5f68bb7b058693bd81b5e9a48d0
Willy De Keyser <spring.boot.programming@gmail.com>
2026-05-19 21:34:36 +0200

    Setup

---
+++ Spring-Boot-Security-AuthenticationProvider-part27/src/main/java/willydekeyser/SpringBootSecurityAuthenticationProviderPart27Application.java
@@ -0,0 +1,13 @@
+package willydekeyser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityAuthenticationProviderPart27Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecurityAuthenticationProviderPart27Application.class, args);
+	}
+
+}
---
+++ Spring-Boot-Security-AuthenticationProvider-part27/src/test/java/willydekeyser/SpringBootSecurityAuthenticationProviderPart27ApplicationTests.java
@@ -0,0 +1,13 @@
+package willydekeyser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecurityAuthenticationProviderPart27ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
